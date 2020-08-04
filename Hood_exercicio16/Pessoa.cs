b074015792ce4144b18bad350a0b95f734ec6679
Carlos Vieira <cadu.vieira2276@gmail.com>
2020-08-04 17:11:56 -0300

    Projeto iniciado mostrando a mensagem aceita e nao aceita, de acordo com o que o usuario digitou

---
+++ Hood_exercicio16/Pessoa.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hood_exercicio16 {
+    class Pessoa {
+        private string _nome;
+        private char _sexo = 'f';
+        private int _idade;
+
+        public Pessoa(string nome, char sexo, int idade) {
+            _nome = nome;
+            _sexo = sexo;
+            _idade = idade;
+        }
+
+        public string Nome {
+            get { return _nome;
+            }
+            set { 
+                if(value != null && value.Length > 1) {
+                    _nome = value;
+                }
+                
+            }
+        }
+        
+        public char Sexo {
+            get { return _sexo; }
+            set { _sexo = value; }
+        }
+
+        public int Idade {
+            get { return _idade; }
+            set { _idade = value; }
+        }
+
+        
+
+        public bool AceitarOuNaoAceitar() {
+            if (_sexo == 'f' && _idade < 25) {
+                Console.WriteLine("Aceita");
+                Console.WriteLine("Nome " + Nome + "," +  " Sexo " + Sexo + "," +  " Idade " + Idade);
+                return true;
+            }
+
+            if(_sexo == 'F' && _idade < 25){
+                Console.WriteLine("Aceita");
+                Console.WriteLine("Nome " + Nome + "," +  " Sexo " + Sexo + "," +  " Idade " + Idade);
+                return true;
+            }
+
+            else {
+                Console.WriteLine("Não aceita");
+                Console.WriteLine("Nome " + Nome + "," +  " Sexo " + Sexo + "," +  " Idade " + Idade);
+                return false;
+            }
+
+        }
+
+       
+
+
+
+    }
+}
---
+++ Hood_exercicio16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hood_exercicio16 {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Pessoa> list = new List<Pessoa>();
+
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite seu sexo: ");
+            char sexo = char.Parse(Console.ReadLine());
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            list.Add(new Pessoa(nome, sexo, idade));
+            
+            Console.WriteLine();
+            
+            Pessoa p = new Pessoa(nome, sexo, idade);
+            p.AceitarOuNaoAceitar();
+
+        }
+
+    }
+}
